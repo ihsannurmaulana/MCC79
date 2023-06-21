2f6d96904bf4d96c0074d2b80dc9afe0f5fcdd42
ihsannurmaulana <maulanaihsannur@gmail.com>
2023-06-21 22:16:25 +0700

    Remove account

--- API/Models/AccountRole.cs
+++ API/Models/AccountRole.cs
@@ -1,14 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Models;
 
 [Table("tb_tr_account_roles")]
 public class AccountRole : BaseEntity
 {
-    [Key]
-    [Column("guid")]
-    public Guid Guid { get; set; }
 
     [Column("account_guid")]
     public Guid AccountGuid { get; set; }
